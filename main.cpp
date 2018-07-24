b61fc1e43ae13b223f7ba94c4d4adf58b03ab7ef
Dylan Kreth <dylankreth@gmail.com>
2018-07-24 11:55:33 -0500

    more results files

--- main.cpp
+++ main.cpp
@@ -36,9 +36,9 @@
 
 #define SERIAL_NUMBER_FILE "/home/pi/serial.txt"
 
-//#define SPI_TEST
-//#define COMBO_TEST
+#define SPI_TEST
 #define MEM_TEST
+#define COMBO_TEST
 
 #define TRANSMITLEN (10000)
 #define NUM_OPERATIONS (10000)
@@ -72,8 +72,8 @@ int main()
 
 #ifdef SPI_TEST
     setup_spi();
-    ofstream fileStream;
-    fileStream.open("/media/pi/UPTIMEDRIVE1/pi_test/SPI_TEST_results.txt",ios::out);
+    ofstream spi_fileStream;
+    spi_fileStream.open("/home/pi/pi_test/test_results/SPI_TEST_results3.txt",ios::out);
     char send[TRANSMITLEN] = {};
     char received[TRANSMITLEN]={};
     for(int i=0;i<TRANSMITLEN;i++){
@@ -83,7 +83,7 @@ int main()
     struct timeval spi_end_time;
 
     for (int j=1; j<TRANSMITLEN; j*=2){
-        fileStream << "block size " << j << endl;
+        spi_fileStream << "block size " << j << endl;
         gettimeofday(&spi_start_time,NULL);
         for (int i=0; i<NUM_OPERATIONS; i++){
               bcm2835_spi_transfernb(send,received,j);
@@ -95,22 +95,22 @@ int main()
 
         float megabitsPerSec = bitsPerSec/1000000;
         float megabytesPerSec = megabitsPerSec/8.0;
-        fileStream << " Mb/s: " << megabitsPerSec << endl;
-        fileStream << " MB/s: " << megabytesPerSec << endl;
+        spi_fileStream << " Mb/s: " << megabitsPerSec << endl;
+        spi_fileStream << " MB/s: " << megabytesPerSec << endl;
         float realToTheoretical = megabitsPerSec/50;
-        fileStream << "realToTheoretical " << realToTheoretical << endl;
+        spi_fileStream << "realToTheoretical " << realToTheoretical << endl;
     }
 
-    fileStream.close();
+    spi_fileStream.close();
     bcm2835_spi_end();
     bcm2835_close();
 #endif // SPI_TEST
 
 #ifdef MEM_TEST
 
-    ofstream resultsFile;
-    resultsFile.open("/home/pi/pi_test/test_results/MEM_TEST_results.txt",ios::out);
-    ofstream fileStream;// for writing binary files
+    ofstream mem_resultsFile;
+    mem_resultsFile.open("/home/pi/pi_test/test_results/MEM_TEST_results3.txt",ios::out);
+    ofstream mem_fileStream;// for writing binary files
 
     int file_count_iterator = 0;
     char fileName[64];
@@ -122,60 +122,54 @@ int main()
     struct timeval mem_start_time;
     struct timeval mem_end_time;
     for(;num_files<257; num_files*=2,file_size/=2){
-        //cout << num_files << " " << file_size << "MB files" << endl;
-        //cout << "write file" << endl;
 
         gettimeofday(&mem_start_time,NULL);
-        //cout << "cloc begin: " << mem_begin_time << endl;
         for(int k=0;k<num_files;k++){//number of files
             file_count_iterator++;
-//            stringstream fileName;
-//            fileName << "/media/pi/UPTIMEDRIVE1/test" << file_count_iterator << ".bin";
             sprintf(fileName,"/media/pi/UPTIMEDRIVE1/test_output_bins/test%d.bin",file_count_iterator);
-            fileStream.open(fileName,ios::out|ios::binary);
+            mem_fileStream.open(fileName,ios::out|ios::binary);
             for(int j=0;j<file_size*1024;j++){//size of each file
-                fileStream.write(buffer,BUFSIZE);
+                mem_fileStream.write(buffer,BUFSIZE);
             }
-            fileStream.close();
+            mem_fileStream.close();
         }
         gettimeofday(&mem_end_time,NULL);
-        //cout << "clock end: " << mem_end_time << endl;
         float mem_timeElapsed = ((mem_end_time.tv_sec*1000000.0+mem_end_time.tv_usec)-(mem_start_time.tv_sec*1000000.0+mem_start_time.tv_usec))/1000000.0;
         cout << "It took " << mem_timeElapsed << " seconds to write " << num_files << " " << file_size << "MB files, which is " << 256*8/mem_timeElapsed<< "Mbits per second." << endl;
-        resultsFile << "It took " << mem_timeElapsed << " seconds to write " << num_files << " " << file_size << "MB files, which is " << 256*8/mem_timeElapsed<< "Mbits per second." << endl;
+        mem_resultsFile << "It took " << mem_timeElapsed << " seconds to write " << num_files << " " << file_size << "MB files, which is " << 256*8/mem_timeElapsed<< "Mbits per second." << endl;
     }
-    resultsFile.close();
+    mem_resultsFile.close();
 
 #endif // MEM_TEST
 
 #ifdef COMBO_TEST
-    ofstream resultsFile;
-    resultsFile.open("/home/pi/pi_test/test_results/COMBO_TEST_results.txt",ios::out);
+    ofstream combo_resultsFile;
+    combo_resultsFile.open("/home/pi/pi_test/test_results/COMBO_TEST_results3.txt",ios::out);
     setup_spi();
-    char send[SPI_CALL_LENGTH]={};
-    char received[SPI_CALL_LENGTH]={};
+    char combo_send[SPI_CALL_LENGTH]={};
+    char combo_received[SPI_CALL_LENGTH]={};
     for (int i=0; i<SPI_CALL_LENGTH; i++){//fill the array to send through the SPI
-        send[i]=i;
+        combo_send[i]=i;
     }
     float speeds[NUMBER_OF_TRIALS];
-    char fileName[64];
+    char combo_fileName[64];
     struct timeval combo_start_time;
     struct timeval combo_end_time;
     for(int trialNumber=0;trialNumber<NUMBER_OF_TRIALS; trialNumber++){
         gettimeofday(&combo_start_time,NULL);
-        ofstream myFile;
-        sprintf(fileName,"/media/pi/UPTIMEDRIVE1/test_output_bins/test%d.bin",trialNumber);
-        myFile.open(fileName,ios::out|ios::binary);
+        ofstream combo_fileStream;
+        sprintf(combo_fileName,"/media/pi/UPTIMEDRIVE1/test_output_bins/test%d.bin",trialNumber);
+        combo_fileStream.open(combo_fileName,ios::out|ios::binary);
         for(int i=0;i<NUM_SPI_CALLS;i++){
-            bcm2835_spi_transfernb(send, received, SPI_CALL_LENGTH);
-            myFile.write(received,SPI_CALL_LENGTH);
+            bcm2835_spi_transfernb(combo_send, combo_received, SPI_CALL_LENGTH);
+            combo_fileStream.write(combo_received,SPI_CALL_LENGTH);
         }
-        myFile.close();
+        combo_fileStream.close();
         gettimeofday(&combo_end_time,NULL);
         float combo_timeElapsed = ((combo_end_time.tv_sec*1000000.0+combo_end_time.tv_usec)-(combo_start_time.tv_sec*1000000.0+combo_start_time.tv_usec))/1000000.0;
         float megabitsPerSec = SPI_CALL_LENGTH*NUM_SPI_CALLS/combo_timeElapsed/1024/1024*8;
         cout << "trial " << trialNumber << ": " << megabitsPerSec << " Mb/s" << endl;
-        resultsFile << megabitsPerSec << endl;
+        combo_resultsFile << megabitsPerSec << endl;
         speeds[trialNumber] = megabitsPerSec;
     }
     float averageSpeed=0;
@@ -184,8 +178,8 @@ int main()
     }
     averageSpeed/=NUMBER_OF_TRIALS;
     cout <<"average speed: " << averageSpeed << "Mb/s"<<endl;
-    resultsFile <<"average speed: " << averageSpeed << "Mb/s"<<endl;
-    resultsFile.close();
+    combo_resultsFile <<"average speed: " << averageSpeed << "Mb/s"<<endl;
+    combo_resultsFile.close();
 
         bcm2835_spi_end();
         bcm2835_close();
